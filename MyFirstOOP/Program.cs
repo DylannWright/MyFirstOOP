79d6eaabe9775dbb1159da5463edc01f1808f237
Dylan Wright <17WrighD@fulford.york.sch.uk>
2022-09-21 10:15:17 +0100

    Add project files.

---
+++ MyFirstOOP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstOOP
+{
+    class Student
+    {
+        public string name;
+        public int age;
+        private bool atSchool = false;
+        public void showInfo()
+        {
+            Console.WriteLine(name + " " + age);
+        }
+        public void toggleAtSchool()
+        {
+            atSchool = !(atSchool);
+
+        }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student roald = new Student();
+            Student stuart = new Student();
+            roald.name = "Roald";
+            roald.age = 14;
+            stuart.name = "Stuart";
+            stuart.age = 16;
+            roald.showInfo();
+            stuart.showInfo();
+            roald.toggleAtSchool();
+
+
+
+
+            
+        }
+    }
+}
